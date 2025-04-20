6bbff45ea204268dd07432e539ea3fd8293701f5
Vincent Dixon <vincentedward007@gmail.com>
2025-04-20 10:16:34 -0700

    Added refactored loadData to pull 500 records; Added autoSearch function for search area

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
+const searchResults = document.getElementById("TickerSearchResults");
+var searchResultIDs = [];
+
 const loadData = (fetchindex, dataObj) => {
-    if (fetchindex < 2000) {
+    if (fetchindex < 500) {
         fetch(`https://api.coinlore.net/api/tickers?start=${fetchindex}&limit=100`)
         .then(rawdata => rawdata.json())
         .then(json => {
             for (coinObj of json.data) {
-                dataObj[coinObj.symbol] = {
+                dataObj.push({
+                    rank: coinObj.rank,
                     name: coinObj.name,
-                    id: coinObj.id
-                };
+                    symbol: coinObj.symbol,
+                    id: coinObj.id,
+                    price: coinObj.price_usd
+                });
             }
-            loadData(fetchindex + 100, dataObj)
+                loadData(fetchindex + 100, dataObj)
         })
         .catch(err => console.log(err))
     }
     else {
-        localStorage.setItem(CoinData, JSON.stringify(dataObj))
+        localStorage.setItem("CoinData", JSON.stringify(dataObj))
     }
 }
 
-loadData(0, {})
+const autoSearch = (eventObj) => {
+    let input = eventObj.target.value;
+    searchResults.innerHTML = ""
+    if (!input) {return}
+
+    for (coin of JSON.parse(localStorage.getItem("CoinData")).filter((elem) => {
+        return elem.symbol.indexOf(input) != -1;
+    })) {
+        let searchItem = document.createElement("li");
+        searchItem.innerText = coin.symbol;
+        searchResults.appendChild(searchItem);
+    }
+}
+
+loadData(0, [])
+document.addEventListener("input", autoSearch)
